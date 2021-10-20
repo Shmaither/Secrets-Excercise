6695cdbdb0f9ab7ba41bbd103ce292d27fcb36bf
Shmaither <shmaither@gmail.com>
2021-10-19 20:34:59 -0600

    Level 3 security and authorization: hashing function md5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,10 +19,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-// process.env.<VARIABLE_NAME> for accesing variables on the .env file.
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -42,7 +38,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save((err) => {
@@ -52,7 +48,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   let foundUser = {};
 
   try {
